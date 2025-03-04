480076dff9c5b367c7e07faab86de33efa4b9488
rubsnewgate <sart.rubi98@gmail.com>
2025-03-04 14:24:04 -0600

    Refactor JavaScript code and improve UI
    - Refactored JavaScript to simplify event handling and game logic
    - Improved UI with a clean design and responsive layout
    - Added scoreboard and result display for better user experience

--- main.js
+++ main.js
@@ -1,90 +1,99 @@
-// scores
+const domElements = {
+    // game buttons
+    gameBtns: document.querySelectorAll('.game-btns button'),
+
+    // UI choose indicators
+    playerSelection: document.querySelector('.player-selection'),
+    cpuSelection: document.querySelector('.cpu-selection'),
+
+    // victory counters and combat result
+    playerVictoryCounter: document.querySelector('.player-counter'),
+    cpuVictoryCounter: document.querySelector('.cpu-counter'),
+    combatResult: document.querySelector('.result')
+}
+
+// initial scores
 let playerScore = 0
 let cpuScore = 0
 
-// cpu choice
-const randomNumber = () => Math.floor(Math.random() * 3) + 1
+// player and cpu selection
 
-function getComputerChoise (number) {
-    let choice = ''
+function getComputerSelection () {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
 
-    switch (number) {
-        case 1:
-            choice = 'rock'
-            break
-        case 2:
-            choice = 'paper'
-            break
-        case 3:
-            choice = 'scissors'
-            break
-        default:
-            choice = 'Error: Something goes wrong, please try it later.'
-            break
+    switch (randomNumber) {
+        case 1: return 'rock'
+        case 2: return 'paper'
+        case 3: return 'scissors'
+        default: return 'unknown'
     }
-
-    return choice
-}
-
-// player choice
-function getHumanChoise () {
-    let choice = prompt('Please give us your choice: ')
-
-    if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
-        choice = prompt('Invalid input! Please select rock, paper or scissors: ')
-    }
-
-    const formattedChoise = choice.toLowerCase()
-    return formattedChoise
 }
 
 // battle algorithm
 function roundBattle (playerSelection, cpuSelection) {
+    const playerVictories = domElements.playerVictoryCounter
+    const cpuVictories = domElements.cpuVictoryCounter
+    const combatResult = domElements.combatResult
+
     if (
         playerSelection === 'rock' && cpuSelection === 'scissors' ||
         playerSelection === 'scissors' && cpuSelection === 'paper' ||
         playerSelection === 'paper' && cpuSelection === 'rock'
     ) {
         playerScore++
-        console.log('Player wins!')
-        console.log('\n')
+        playerVictories.textContent = playerScore
+        combatResult.textContent = 'Player Wins!'
     }
     else if (playerSelection === cpuSelection) {
-        console.log('Tied!')
-        console.log('\n')
+        combatResult.textContent = 'Tied!'
     }
     else {
         cpuScore++
-        console.log('CPU wins!')
-        console.log('\n')
+        cpuVictories.textContent = cpuScore
+        combatResult.textContent = 'CPU Wins!'
+    }
+
+    checkWonRounds()
+}
+
+function checkWonRounds () {
+    const finalResult = domElements.combatResult
+
+    if (playerScore === 5) {
+        finalResult.textContent = 'Congrats! You\'re the absolute winner!'
+
+        domElements.gameBtns.forEach((button) => {
+            button.disabled = true
+        })
+
+        return
+    }
+    else if (cpuScore === 5) {
+        finalResult.textContent = 'What a shame! You loose, try again later.'
+
+        domElements.gameBtns.forEach((button) => {
+            button.disabled = true
+        })
+
+        return
     }
 }
 
-// game init
-function playRound () {
-    const cpuChoice = getComputerChoise(randomNumber())
-    const playerChoice = getHumanChoise()
+// play a round
+function playRound (playerSelection) {
+    const cpuSelection = getComputerSelection()
 
-    console.log(`CPU choice: ${cpuChoice}`)
-    console.log(`Player choice: ${playerChoice}`)
+    domElements.playerSelection.textContent = playerSelection
+    domElements.cpuSelection.textContent = cpuSelection
 
     // print the winner
-    return roundBattle(playerChoice, cpuChoice)
+    return roundBattle(playerSelection, cpuSelection)
 }
 
-function playGame () {
-    while (playerScore < 5 && cpuScore < 5) {
-        playRound()
-        if (playerScore === 5) {
-            console.log('Congratulations, you win!')
-        }
-        else if (cpuScore === 5) {
-            console.log('What a shame, you\'re defeted!')
-        }
-    }
-}
-
-// waiting to render all the page content
-window.onload = () => {
-    playGame()
-}
+// init round
+domElements.gameBtns.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const playerSelection = event.target.id
+        playRound(playerSelection)
+    })
+})
